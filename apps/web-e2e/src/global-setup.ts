2bb255372273d81a05160545bc11f7aca28c45a9
bartdelange <bart.de.lange32@gmail.com>
2026-09-04 16:49:10 +0200

    🐛 fix(repo): DB path does not exist in CI runs

--- apps/web-e2e/src/global-setup.ts
+++ apps/web-e2e/src/global-setup.ts
@@ -1,8 +1,10 @@
-import { rm } from 'node:fs/promises';
-import { resolve } from 'node:path';
+import { mkdir, rm } from 'node:fs/promises';
+import { dirname, resolve } from 'node:path';
 
 export default async function globalSetup(): Promise<void> {
   const databasePath = resolve('apps/web-e2e/test-output/e2e.sqlite');
+
+  await mkdir(dirname(databasePath), { recursive: true });
 
   await Promise.all([
     rm(databasePath, {
